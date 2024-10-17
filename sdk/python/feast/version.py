94950693eae3be15e6028f384c89ca62b718bb5b
Rob Howley <howley.robert@gmail.com>
2024-10-17 11:53:50 -0400

    hardcode version so we can run the branch

--- sdk/python/feast/version.py
+++ sdk/python/feast/version.py
@@ -3,8 +3,9 @@ from importlib.metadata import PackageNotFoundError, version
 
 def get_version():
     """Returns version information of the Feast Python Package."""
-    try:
-        sdk_version = version("feast")
-    except PackageNotFoundError:
-        sdk_version = "unknown"
-    return sdk_version
+    return "0.41.0"
+    # try:
+    #     sdk_version = version("feast")
+    # except PackageNotFoundError:
+    #     sdk_version = "unknown"
+    # return sdk_version
